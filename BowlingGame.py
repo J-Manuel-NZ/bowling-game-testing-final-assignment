0acad8f22098112d72a85ca6acadf7746e7e56a0
Justin Manuel <112037970+J-Manuel-NZ@users.noreply.github.com>
2023-12-05 13:58:31 +1300

    Code Refactor
     + Added spaces within the functions to increase readability
    + renamed functions to make their function in the code more obvious
    + added comments

--- BowlingGame.py
+++ BowlingGame.py
@@ -3,7 +3,7 @@
 
 class BowlingGame:
     def __init__(self):
-        self.rolls=[]
+        self.rolls = []
 
     def roll(self, pins):
         """Append scores to list
@@ -28,15 +28,18 @@ class BowlingGame:
         result = 0
         rollIndex = 0
         for frameIndex in range(10):
-            if self.isStrike(rollIndex):
-                result += self.strikeScore(rollIndex) # re-named to strikeScore (there was a spelling error)
-                rollIndex +=1 
+            # if roll is a 10 then it is a strike and play would move to the next frame so rollIndex increases by 1
+            if self.isStrike(rollIndex): 
+                result += self.calculateStrikeScore(rollIndex) # re-named to calculateStrikeScore (there was a spelling error)
+                rollIndex += 1 
+            # if roll is not 10 then check if this roll + the next roll = spare, rollIndex increases by 2
             elif self.isSpare(rollIndex):
-                result += self.spareScore(rollIndex)
-                rollIndex +=2 
+                result += self.calculateSpareScore(rollIndex)
+                rollIndex += 2 
+            # finally, if roll is not spare then add this roll to the next and move to next frame, rollIndex increases by 2
             else:
-                result += self.frameScore(rollIndex)
-                rollIndex +=2 
+                result += self.calculateFrameScore(rollIndex)
+                rollIndex += 2 
         return result
 
     def isStrike(self, rollIndex):
@@ -62,7 +65,7 @@ class BowlingGame:
         """
         return self.rolls[rollIndex] + self.rolls[rollIndex+1] == 10
     
-    def strikeScore(self, rollIndex): # re-named to strikeScore (there was a spelling error)
+    def calculateStrikeScore(self, rollIndex): # re-named to calculateStrikeScore (there was a spelling error)
         """Calculate strike score
         
         Args: 
@@ -71,9 +74,9 @@ class BowlingGame:
         Returns:
             int: returns the score of the strike, 10 + the following 2 rolls
         """
-        return  10 + self.rolls[rollIndex+1] + self.rolls[rollIndex+2]
+        return  10 + self.rolls[rollIndex + 1] + self.rolls[rollIndex + 2]
 
-    def spareScore(self,rollIndex):
+    def calculateSpareScore(self, rollIndex):
         """Calculate spare score
         
         Args: 
@@ -82,9 +85,9 @@ class BowlingGame:
         Returns:
             int: returns the score of the spare, 10 + the following roll
         """
-        return  10 + self.rolls[rollIndex+2]
+        return  10 + self.rolls[rollIndex + 2]
 
-    def frameScore(self, rollIndex):
+    def calculateFrameScore(self, rollIndex):
         """Calculate frame score
         
         Args: 
